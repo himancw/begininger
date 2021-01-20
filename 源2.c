d0d03c4b12a3e8a78658b475c894df075b4dee65
himancw <72565306+himancw@users.noreply.github.com>
2021-01-20 20:47:07 +0800

    Add files via upload
    scanf()

---
+++ 源2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{   
+	double a;
+	int b,c;
+	if (scanf("%lf %d %d", &a, &b, &c) == 3)//The returned value of "scanf()" is equal to the number of the truely readed characters.
+		printf("you are right!");
+	else
+		printf("you are wrong");
+}  
