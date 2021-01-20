98c8fc050cf5d7bd11130269635d3a81d11b8076
himancw <72565306+himancw@users.noreply.github.com>
2021-01-20 20:48:59 +0800

    Update and rename 源2.c to practice4.c

--- practice4.c
+++ practice4.c
@@ -6,5 +6,5 @@ main()
 	if (scanf("%lf %d %d", &a, &b, &c) == 3)//The returned value of "scanf()" is equal to the number of the truely readed characters.
 		printf("you are right!");
 	else
-		printf("you are wrong");
+		printf("you are wrong!");
 }  
