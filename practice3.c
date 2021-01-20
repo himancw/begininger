3894470dea5c4afbb8bfc9b4fd314dd49435fa06
himancw <72565306+himancw@users.noreply.github.com>
2021-01-20 14:11:57 +0800

    Add files via upload

---
+++ practice3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	a = printf("MAKE ""CHINA"//The returned value of "printf()" is assigned to the variate"a".
+		" GREAT"" AGAIN!\n");
+	    printf("MAKE CHINA\
+ GREAT AGAIN!\n");//The codes of the last row are bound to follow the codes from the begining of next row.
+		printf("%d", a);
+} 
