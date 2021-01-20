1922f098d7d23967401498098e53359a8fe47ca2
himancw <72565306+himancw@users.noreply.github.com>
2021-01-21 00:31:44 +0800

    Update practice1.c

--- practice1.c
+++ practice1.c
@@ -4,4 +4,8 @@ main()
 	char ch;
 	while ((ch = getchar()) != '\n')
 		printf("%c", ch+1);
+	//The "getchar()" in the loop body can continuously read every word separated by " "(space) but the "scanf()" is adverse to it.
+	char str[30];
+	(void)scanf("%s", str);
+	printf("%s", str);
 }
