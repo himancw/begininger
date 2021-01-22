4a51574fb987f35b9b7e1de8eeb90104fef257ce
himancw <72565306+himancw@users.noreply.github.com>
2021-01-22 12:42:55 +0800

    Update practice2.c

--- practice2.c
+++ practice2.c
@@ -12,4 +12,10 @@ main()
 	printf("[%7.4f]\n",3.1415926);
 	printf("[%010.4f]\n", 3.1415926);
 	printf("[%12.4e]\n", 3.1415926*1.e7);
+	printf("%#4x\n", 100);
+	printf("%#4o\n", 100);
+	printf("%04d\n",0144 );
+	printf("%04d\n", 0x64);
+	printf("%04c\n",'\144');
+	printf("%04c\n", '\x64');
 } 
