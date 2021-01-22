354e1d04157558e5418ab05d4558fc24a6e3a498
himancw <72565306+himancw@users.noreply.github.com>
2021-01-22 18:07:39 +0800

    Update practice4.c

--- practice4.c
+++ practice4.c
@@ -3,7 +3,7 @@ main()
 {   
 	double a;
 	int b,c;
-	if (scanf("%lf %d %d", &a, &b, &c) == 3)//The returned value of "scanf()" is equal to the number of the truely readed characters.
+	if (scanf("%lf %d %d", &a, &b, &c) == 3)            //The returned value of "scanf()" is equal to the number of the truely readed characters.
 		printf("you are right!");
 	else
 		printf("you are wrong!");
