01491987895c0b31ea47cef44f116040e81b42be
himancw <72565306+himancw@users.noreply.github.com>
2021-01-22 18:03:40 +0800

    Update practice3.c

--- practice3.c
+++ practice3.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 main()
 {
-	int a;
-	a = printf("MAKE ""CHINA"//The returned value of "printf()" is assigned to the variate"a".
-		" GREAT"" AGAIN!\n");
-	    printf("MAKE CHINA\
- GREAT AGAIN!\n");//The codes of the last row are bound to follow the codes from the begining of next row.
-		printf("%d", a);
+int a;
+a = printf("MAKE ""CHINA"           //The returned value of "printf()" is assigned to the variate"a".
+    " GREAT"" AGAIN!\n");
+	
+printf("MAKE CHINA\
+  GREAT AGAIN!\n");                 //The codes of the last row are bound to follow the codes from the begining of next row.
+	
+printf("%d", a);
 } 
