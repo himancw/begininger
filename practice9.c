b95257bf9740cae583e57e1b041fd9541dcc71d6
himancw <72565306+himancw@users.noreply.github.com>
2021-01-30 17:58:52 +0800

    Create practice9.c

---
+++ practice9.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+	int num=0;
+	for (printf("enter a number:"); num != 99;)  //The first expression not necessarily be assigning initial value for variate.
+	(void)scanf("%d", &num);
+	printf("you are right!");
+}
